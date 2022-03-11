d1cb877f0a33c8d66f68c6e0f351e987d3d03d80
baegteun <baegteun@gmail.com>
2022-03-11 14:29:22 +0900

    Initial Commit

---
+++ Counter-MVVM/Counter_MVVMApp.swift
@@ -0,0 +1,17 @@
+//
+//  Counter_MVVMApp.swift
+//  Counter-MVVM
+//
+//  Created by 최형우 on 2022/03/11.
+//
+
+import SwiftUI
+
+@main
+struct Counter_MVVMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
